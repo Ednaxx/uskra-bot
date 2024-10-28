20296cbfcbc734521fe2f7451825fe585899ead4
Alexandre Morais <xandre2setembro2002@gmail.com>
2024-10-28 13:55:45 -0300

    refactor: removed comments from app.py

--- app.py
+++ app.py
@@ -44,42 +44,6 @@ class Bot:
             else:
                 logging.info(f' {event.d['member']['nick']} left channel')
 
-    # IN
-    ''' 
-    GatewayMessage(op=0, 
-        d={'member': 
-            {'user': 
-                {
-                'username': 'ednax', 'public_flags': 0, 'id': '358350385192108032',
-                'global_name': 'Ednax', 'display_name': 'Ednax', 'discriminator': '0', 
-                'clan': None, 'bot': False, 'avatar_decoration_data': None, 'avatar': '24ccc17eb04d47dff595aaa19469afa9'
-                }, 
-            'roles': ['1076319496312213504', '1261023886234091522', '903836023581200484', '904900617418457108'],
-            'premium_since': None, 'pending': False, 'nick': 'Wandinha', 'mute': False, 'joined_at': '2021-09-03T02:33:47.147000+00:00',
-            'flags': 0, 'deaf': False, 'communication_disabled_until': None, 'banner': None, 'avatar': None
-        }, 
-        'user_id': '358350385192108032', 'suppress': False, 'session_id': '7288eeb38df2ed4548010a22713cb42e', 
-        'self_video': False, 'self_mute': True, 'self_deaf': False, 'request_to_speak_timestamp': None, 'mute': False, 
-        'guild_id': '811761295564734524', 'discoverable': True, 'deaf': False, 'channel_id': '1193256214985982003'}, s=4, t='VOICE_STATE_UPDATE')
-    '''
-    # OUT
-    '''
-    GatewayMessage(op=0, 
-        d={'member': 
-            {'user': 
-                {
-                'username': 'ednax', 'public_flags': 0, 'id': '358350385192108032', 
-                'global_name': 'Ednax', 'display_name': 'Ednax', 'discriminator': '0', 'clan': None, 'bot': False, 
-                'avatar_decoration_data': None, 'avatar': '24ccc17eb04d47dff595aaa19469afa9'
-                }, 
-            'roles': ['1076319496312213504', '1261023886234091522', '903836023581200484', '904900617418457108'], 
-            'premium_since': None, 'pending': False, 'nick': 'Wandinha', 'mute': False, 'joined_at': '2021-09-03T02:33:47.147000+00:00', 
-            'flags': 0, 'deaf': False, 'communication_disabled_until': None, 'banner': None, 'avatar': None
-        }, 
-        'user_id': '358350385192108032', 'suppress': False, 'session_id': '7288eeb38df2ed4548010a22713cb42e', 
-        'self_video': False, 'self_mute': True, 'self_deaf': False, 'request_to_speak_timestamp': None, 'mute': False, 
-        'guild_id': '811761295564734524', 'discoverable': True, 'deaf': False, 'channel_id': None}, s=5, t='VOICE_STATE_UPDATE') 
-    '''
     def treat_commands(self, event: GatewayMessage):
         message = event.d["content"].split()
         channel = event.d["channel_id"]
--- env.py
+++ env.py
@@ -8,5 +8,5 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 API_VERSION = "v10"
 BASE_URL = f"https://discord.com/api/{API_VERSION}"
 
-BOT_NAME = "Fat Marcos"
-APP_NAME = "Uskra Bot"
+BOT_NAME = os.getenv("BOT_NAME")
+APP_NAME = os.getenv("APP_NAME")
